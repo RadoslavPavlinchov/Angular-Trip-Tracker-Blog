15b1ba87bfa25bae13273b2a23e0b0146101d664
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-02-16 07:06:29 +0200

    profile page functionality

--- trip-tracker-blog/src/app/components/admin/profile/profile.component.ts
+++ trip-tracker-blog/src/app/components/admin/profile/profile.component.ts
@@ -1,4 +1,9 @@
+import { FileI } from "./../../../shared/models/file.interface";
+import { UserI } from "./../../../shared/models/user.interface";
+import { AuthService } from "./../../../shared/services/auth.service";
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public image: FileI;
+  public currentImage: string = 'https://picsum.photos/seed/picsum/150/150';
+
+  constructor(private authService: AuthService) { }
+
+  public profileForm = new FormGroup({
+    displayName: new FormControl('', Validators.required),
+    email: new FormControl({ value: '', disabled: true }, Validators.required),
+    photoURL: new FormControl('', Validators.required)
+  });
 
   ngOnInit() {
+    this.authService.userData$.subscribe(user => {
+      this.initValuesForm(user);
+    })
   }
 
+  onSaveUser(user: UserI): void {
+    this.authService.preSaveUserProfile(user, this.image);
+  }
+
+  private initValuesForm(user: UserI): void {
+    if (user.photoURL) {
+      this.currentImage = user.photoURL;
+    }
+
+    this.profileForm.patchValue({
+      displayName: user.displayName,
+      email: user.email,
+
+    })
+  }
+
+  imageHandler(image:FileI): void {
+    this.image = image;
+  }
 }
--- trip-tracker-blog/src/app/shared/services/auth.service.ts
+++ trip-tracker-blog/src/app/shared/services/auth.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { UserI } from "../models/user.interface";
 import { AngularFireAuth} from "@angular/fire/auth";
 import { Observable } from "rxjs";
+import { FileI } from '../models/file.interface';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  public userData: Observable<firebase.User>;
+  public userData$: Observable<firebase.User>;
+  public filePath: string;
 
-  constructor(private afAuth: AngularFireAuth) {
-    this.userData = afAuth.authState;
+  constructor(private afAuth: AngularFireAuth, private storage: AngularFireStorage) {
+    this.userData$ = afAuth.authState;
    }
 
   loginWithEmail(user: UserI) {
@@ -20,5 +24,37 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  preSaveUserProfile(user: UserI, image?: FileI): void {
+    if(image) {
+      this.uploadImage(user, image);
+    } else {
+      this.saveUserProfile(user);
+    }
+  }
+
+  private uploadImage(user: UserI, image: FileI): void {
+    this.filePath = `images/${image.name}`;
+    const fileRef = this.storage.ref(this.filePath);
+    const task = this.storage.upload(this.filePath, image);
+    task.snapshotChanges()
+    .pipe(
+      finalize(() => {
+        fileRef.getDownloadURL().subscribe( urlImage => {
+          user.photoURL = urlImage;
+          this.saveUserProfile(user);
+        });
+      })
+    ).subscribe();
+  }
+
+  private saveUserProfile(user: UserI) {
+    this.afAuth.auth.currentUser.updateProfile({
+      displayName: user.displayName,
+      photoURL: user.photoURL
+    })
+    .then(() => console.log('User updated'))
+    .catch((err) => console.log('Error', err));
   }
 }
